848f04c7b02186c1f1875ec653f7bf120deab53f
ConstellationHoundDogs <vsv555555@bk.ru>
2013-11-30 10:31:45 +0200

    Last commit

--- src/main/java/constellation/DBOperator.java
+++ src/main/java/constellation/DBOperator.java
@@ -1,5 +1,11 @@
 package constellation;
 
+import constellation.vo.Column;
+import constellation.vo.DBInfo;
+import constellation.vo.Table;
+
+import java.sql.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: vladimir
@@ -9,12 +15,83 @@ package constellation;
 public class DBOperator {
 
     private String dbURL;
+    private Connection conn;
 
     public DBOperator(String dbURL){
         this.dbURL = dbURL;
     }
 
+    public void initDB(){
+        try {
+            conn = DriverManager.getConnection(dbURL);
+        } catch (SQLException e) {
+            DBUtil.logger.error("Couldn't get DB connection: " + e.getMessage());
+        }
+    }
 
+
+    public DBInfo getDbInfo() throws SQLException {
+        DBInfo dbInfo = new DBInfo();
+
+        updateDataBaseMetaData(dbInfo);
+
+        updateTablesContents(dbInfo);
+
+
+        return dbInfo;
+    }
+
+    private void updateTablesContents(DBInfo dbInfo) {
+        Statement statement;
+        ResultSet resultSet;
+        for(Table t : dbInfo.tables){
+            try {
+                statement = conn.createStatement();
+                resultSet = statement.executeQuery("SELECT * FROM " + t.tableName);
+                ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+                t.columnsNumber = resultSetMetaData.getColumnCount();
+                for(int i = 1; i <= t.columnsNumber; i++){
+                    Column column = new Column();
+                    column.setColumnName(resultSetMetaData.getColumnName(i));
+                    column.setColumnType(resultSetMetaData.getColumnTypeName(i));
+                    t.columns.add(column);
+                }
+                updateColumnsContent(resultSet, t);
+            } catch (SQLException e) {
+                DBUtil.logger.error("Couldn't get statementQuery: " + e.getMessage());
+            }
+        }
+    }
+
+    private void updateColumnsContent(ResultSet resultSet, Table t) throws SQLException {
+        while(resultSet.next()){
+            for(int i = 0; i < t.columnsNumber; i++){
+                Column column = t.columns.get(i);
+                column.content.add(resultSet.getString(i+1));
+            }
+        }
+    }
+
+    private void updateDataBaseMetaData(DBInfo dbInfo) {
+        DatabaseMetaData databaseMetaData;
+        try {
+            databaseMetaData = conn.getMetaData();
+            dbInfo.dataBaseName = databaseMetaData.getDatabaseProductName();
+            dbInfo.dataBaseVersion = databaseMetaData.getDriverVersion();
+            ResultSet schemas = databaseMetaData.getSchemas();
+            while (schemas.next()){
+                dbInfo.schemaNames.add(schemas.getString(1));
+            }
+            ResultSet tables = databaseMetaData.getTables(null, "APP", null, null);
+            while (tables.next()){
+                Table table = new Table();
+                table.tableName = tables.getString("TABLE_NAME");
+                dbInfo.tables.add(table);
+            }
+        } catch (SQLException e) {
+            DBUtil.logger.error("Couldn't get DataBaseMetaData: " + e.getMessage());
+        }
+    }
 
 
 }
--- src/main/java/constellation/DBUtil.java
+++ src/main/java/constellation/DBUtil.java
@@ -37,8 +37,11 @@ public class DBUtil {
 
     public void run() throws SQLException {
         initLogger();
-        initDB();
-        DBInfo dbInfo = getDbInfo();
+
+        DBOperator dbOperator = new DBOperator(dbURL);
+
+        dbOperator.initDB();
+        DBInfo dbInfo = dbOperator.getDbInfo();
 
         XMLFileCreator xmlFileCreator = new XMLFileCreator();
         xmlFileCreator.createFile(dbInfo);
@@ -46,76 +49,4 @@ public class DBUtil {
         plainTextFileCreator.createFile(dbInfo);
     }
 
-
-
-    private void initDB(){
-        try {
-            conn = DriverManager.getConnection(dbURL);
-        } catch (SQLException e) {
-            logger.error("Couldn't get DB connection: "+ e.getMessage());
-        }
-    }
-
-    private DBInfo getDbInfo() throws SQLException {
-        DBInfo dbInfo = new DBInfo();
-
-        getDataBaseMetaData(dbInfo);
-
-        getTablesContents(dbInfo);
-
-
-        return dbInfo;
-    }
-
-    private void getTablesContents(DBInfo dbInfo) {
-        Statement statement;
-        ResultSet resultSet;
-        for(Table t : dbInfo.tables){
-            try {
-                statement = conn.createStatement();
-                resultSet = statement.executeQuery("SELECT * FROM " + t.tableName);
-                ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-                t.columnsNumber = resultSetMetaData.getColumnCount();
-                for(int i = 1; i <= t.columnsNumber; i++){
-                    Column column = new Column();
-                    column.setColumnName(resultSetMetaData.getColumnName(i));
-                    column.setColumnType(resultSetMetaData.getColumnTypeName(i));
-                    t.columns.add(column);
-                }
-                getColumnsContent(resultSet, t);
-            } catch (SQLException e) {
-                logger.error("Couldn't get statementQuery: " + e.getMessage());
-            }
-        }
-    }
-
-    private void getColumnsContent(ResultSet resultSet, Table t) throws SQLException {
-        while(resultSet.next()){
-            for(int i = 0; i < t.columnsNumber; i++){
-                Column column = t.columns.get(i);
-                column.content.add(resultSet.getString(i+1));
-            }
-        }
-    }
-
-    private void getDataBaseMetaData(DBInfo dbInfo) {
-        DatabaseMetaData databaseMetaData;
-        try {
-            databaseMetaData = conn.getMetaData();
-            dbInfo.dataBaseName = databaseMetaData.getDatabaseProductName();
-            dbInfo.dataBaseVersion = databaseMetaData.getDriverVersion();
-            ResultSet schemas = databaseMetaData.getSchemas();
-            while (schemas.next()){
-                dbInfo.schemaNames.add(schemas.getString(1));
-            }
-            ResultSet tables = databaseMetaData.getTables(null, "APP", null, null);
-            while (tables.next()){
-                Table table = new Table();
-                table.tableName = tables.getString("TABLE_NAME");
-                dbInfo.tables.add(table);
-            }
-        } catch (SQLException e) {
-            logger.error("Couldn't get DataBaseMetaData: " + e.getMessage());
-        }
-    }
 }
